ed3050c3bc2e64268884babdf029b23fa7deb5a0
John Henckel <jdhenckel@gmail.com>
2024-05-18 15:28:35 -0500

    add demos for modal dialog and datagrid

--- Netlify/Website/client.js
+++ Netlify/Website/client.js
@@ -13,11 +13,64 @@ function queryElem(q) {
     return Array.from(document.querySelectorAll(q));
 }
 
+
+// Convert a database row into a table row, 
+// returns a string like "<tr>...."
+function render_row(row,col_names) {
+    let result = '<tr>';
+    result += col_names.map(c => `<td>${row[c]}</td>`).join('');
+    return result + '</tr>';
+}
+
+// Convert a set of database row into a table 
+// returns a string like "<table>...."
+function render_table(id,rows,col_names) {
+    let result = `<table id="${id}">\n`;
+    result += '<tr>';
+    result += col_names.map(c => `<th>${c}</th>`).join('');
+    result + '</tr>\n';
+    result += rows.map(r => render_row(r,col_names)).join('\n');
+    return result + '</table>\n';
+}
+
 function pingHost() {
     return axios.get(api + '/ping').then(response => {
         let tbody = getElem("mydiv");
         tbody.innerHTML = response.data.message;
     });
+}
+
+async function onButton() {
+    let tbody = getElem("mydiv");
+    tbody.innerHTML = 'testing';
+    // //let config = {withCredentials: true};
+    // //axios.defaults.withCredentials = true;
+    // let cred = { email: 'user@test.com', password: '12345'};//, mode: 'cookie' };
+    // let res = await axios.post('http://216.225.199.20:8055/auth/login', cred);
+    // console.log (res);
+    // let tok = res.data.data.access_token;
+    // let cook = res.headers;
+
+    // // let config = {withCredentials: true,
+    // //     headers: { Cookie: 123}};
+    // // console.log(config);   
+    // res = await axios.get('http://216.225.199.20:8055/items/Students?access_token='+tok);
+    let res = await axios.get('http://216.225.199.20:8055/items/Students');
+    
+    //tbody.innerHTML = JSON.stringify(res.data.data);
+    let rows = res.data.data;
+    let col_names = Object.keys(rows[0]);
+    tbody.innerHTML = render_table('students',rows,col_names);
+
+    res = await axios.get('http://216.225.199.20:8055/items/Teachers');
+
+    rows = res.data.data;
+    col_names = Object.keys(rows[0]);
+    let table2 = new DataTable('#teachers', { 
+        data: rows.map(r => col_names.forEach(c => r[c])),
+        columns: col_names
+    });
+    
 }
 
 function onPageLoad() {
--- README.md
+++ README.md
@@ -1,2 +1,99 @@
 # example1
 Example of a fullstack app that uses Netlify and CockroachDB
+
+# To setup netlify
+
+1. login to app.netlify.com
+2. click Add New Site / Import existing project
+3. deploy with GitHub
+4. fill in 
+   
+    - Site name = whatever you want
+    - Base Directory = location of package.json
+    - Build command = (leave empty)
+    - Publish dir = location of index.html
+    - Function dir = location of all your functions (one per .js file)
+
+It will auto deploy from main.  You can disable the auto-deploy and then
+deploy manually after you inspect the preview build.
+
+You can also install Netlify CLI and build/test locally, but I've never tried that.
+
+
+--with-cc-opt='-g
+ -O2
+ -ffile-prefix-map=/build/nginx-zctdR4/nginx-1.18.0=.
+ -flto=auto
+ -ffat-lto-objects
+ -flto=auto
+ -ffat-lto-objects
+ -fstack-protector-strong
+ -Wformat
+ -Werror=format-security
+ -fPIC
+ -Wdate-time
+ -D_FORTIFY_SOURCE=2'
+
+ --with-ld-opt='-Wl,-Bsymbolic-functions
+  -flto=auto
+  -ffat-lto-objects
+  -flto=auto
+  -Wl,-z,relro
+  -Wl,-z,now
+  -fPIC'
+
+ --prefix=/usr/share/nginx
+
+ --conf-path=/etc/nginx/nginx.conf
+
+ --http-log-path=/var/log/nginx/access.log
+
+ --error-log-path=/var/log/nginx/error.log
+
+ --lock-path=/var/lock/nginx.lock
+
+ --pid-path=/run/nginx.pid
+
+ --modules-path=/usr/lib/nginx/modules
+
+ --http-client-body-temp-path=/var/lib/nginx/body
+
+ --http-fastcgi-temp-path=/var/lib/nginx/fastcgi
+
+ --http-proxy-temp-path=/var/lib/nginx/proxy
+
+ --http-scgi-temp-path=/var/lib/nginx/scgi
+
+ --http-uwsgi-temp-path=/var/lib/nginx/uwsgi
+
+ --with-compat
+
+ --with-debug
+
+ --with-pcre-jit
+
+ --with-http_ssl_module
+
+ --with-http_stub_status_module
+
+ --with-http_realip_module
+
+ --with-http_auth_request_module
+
+ --with-http_v2_module
+
+ --with-http_dav_module
+
+ --with-http_slice_module
+
+ --with-threads
+
+ --add-dynamic-module=/build/nginx-zctdR4/nginx-1.18.0/debian/modules/http-geoip2
+
+ --with-http_addition_module
+
+ --with-http_gunzip_module
+
+ --with-http_gzip_static_module
+
+ --with-http_sub_module
