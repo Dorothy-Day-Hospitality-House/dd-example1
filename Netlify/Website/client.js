0707f12fa3c911990ad249fee26cec42d2bbbefa
Michael Fingerson <michael@fingerson.com>
2024-06-11 19:11:25 -0500

    Updated visitor logs - headers

--- Netlify/Website/client.js
+++ Netlify/Website/client.js
@@ -339,10 +339,10 @@ class DDayHouseApp {
             columnDefs: [
               //  {field:"guest_id"},
                 // {field:"visit_id"},
-                {field:"lastname"},
-                {field:"firstname"},
-                {field:"date_of_visit"},
-                {field:"visit_time_description"},
+                {headerName:  'Last Name', field: 'lastname' },
+                {headerName: 'First Name', field: 'firstname' },
+                {headerName: 'Date of Visit', field: 'date_of_visit' },
+                {headerName: 'Visit Time', field: 'visit_time_description' },
             ]
         };        
         const grid = getElem('guest-grid');
