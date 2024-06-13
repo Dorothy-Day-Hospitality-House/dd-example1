3ea7b30f3f01098a5f1179fd6694c15d4b7b6fce
John Henckel <jdhenckel@gmail.com>
2024-06-13 11:39:47 -0500

    new file

---
+++ Netlify/Website/message.js
@@ -0,0 +1,30 @@
+
+
+// This class implements the message bar at the top of the window.
+// Construct it using the DOM element of the bar.
+
+class MessageBar {
+
+    constructor(e) {
+        this.elem = e;
+    }
+
+    // Show a message on the bar.  
+    // duration (in seconds) default 10 seconds
+    // severity 0=info, 1=warning, 2+ = error
+    show(message, duration=10, severity=0) {
+        this.elem.innerHTML = message;
+        this.setColor(severity);
+        setTimeout(()=> this.clear(), duration*1000);
+    }
+
+    clear() {
+        this.elem.innerHTML = '';
+        this.setColor(0);
+    }
+
+    setColor(severity) {
+        this.elem.classList.toggle('warning', severity==1);
+        this.elem.classList.toggle('error', severity>1);
+    }
+}
